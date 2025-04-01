55214ef7645d700050d03df491395089bca796e4
Mister-Hope <mister-hope@outlook.com>
2025-04-01 08:48:11 +0000

    Deploying to resource from @ inNENU/resource@4bcf48d4fb372453772e48d0070344a2f6958cf4 🚀

---
+++ service/settings.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Page Handler
+ *
+ * PHP version 8
+ *
+ * @category  Page
+ * @package   Page
+ * @author    Mr.Hope <zhangbowang1998@gmail.com>
+ * @copyright 2021 Mr.Hope
+ * @license   No License
+ * @link      https://mister-hope.com
+ */
+
+declare(strict_types=1);
+
+require_once 'header/post-json.php';
+
+chdir("../config/");
+
+$data = json_decode(file_get_contents('php://input'));
+
+// FIXME: Fallback
+$appId = $data->appId ?? $data->appID;
+$version = $data->version;
+$filename = $appId . "/" . $version . "/settings.json";
+
+$handle = @fopen($filename, "r");
+if ($handle) {
+  $contents = fread($handle, filesize($filename));
+  fclose($handle);
+  echo $contents;
+} else {
+  echo 'error';
+}
