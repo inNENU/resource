4478510e889fbfb424c9d8b208782130c3e37e3b
Mr.Hope <mister-hope@outlook.com>
2025-07-08 18:25:38 +0800

    feat: improve subsidize

--- scripts/convert-txt.ts
+++ scripts/convert-txt.ts
@@ -1,5 +1,5 @@
 import { copyFileSync, mkdirSync } from "node:fs";
-import { dirname, join } from "node:path";
+import { join } from "node:path";
 
 import { deleteSync } from "del";
 import { getFileList } from "innenu-generator";
@@ -10,18 +10,17 @@ const cwd = process.cwd();
 
 deleteSync("./.text/**");
 
+try {
+  mkdirSync(join(cwd, ".text"));
+} catch {
+  // Ignore error if directory already exists
+}
+
 knowledgeList.forEach((item) => {
-  const oldPath = join(cwd, "knowledge", item);
-  const newPath = join(cwd, ".text", item.replace(/\.md$/, ".txt"));
+  if (!item.includes("/")) return;
 
-  // Ensure the directory exists
-  const newDir = dirname(newPath);
-
-  try {
-    mkdirSync(newDir, { recursive: true });
-  } catch {
-    // Ignore error if directory already exists
-  }
-
-  copyFileSync(oldPath, newPath);
+  copyFileSync(
+    join(cwd, "knowledge", item),
+    join(cwd, ".text", item.replace(/\.md$/, ".txt").replace(/\//g, "-")),
+  );
 });
