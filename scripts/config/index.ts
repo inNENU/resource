dc4ecc3847eb4005f92eaaa598fd96d4c7061ec9
Mister-Hope <mister-hope@outlook.com>
2025-08-01 19:10:03 +0800

    feat: update version

--- scripts/config/index.ts
+++ scripts/config/index.ts
@@ -1,8 +1,6 @@
 import "./env.js";
 
 export const appIdInfo = {
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  "1109559721": process.env.QQ_API_KEY,
   wx33acb831ee1831a5: process.env.WECHAT_API_KEY,
   wx2550e3fd373b79a8: process.env.WE_API_KEY,
   wx2d632391509810f8: process.env.ZAI_API_KEY,
